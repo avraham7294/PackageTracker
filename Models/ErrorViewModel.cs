5dbf45c8d00c983f5658905c16c7b8f9419d9c42
Avraham <Avraham@DESKTOP-7KCTFOE>
2024-12-21 14:48:14 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PackageTracker.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
